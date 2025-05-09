4134dd7cb51e2aa70ba659f69c9fd020c3b92972
dogsdays123 <97099336+dogsdays123@users.noreply.github.com>
2025-05-09 15:38:32 +0900

    ㅇ
    ㅇ

--- KDT/src/main/java/org/zerock/b01/repository/SupplierStockRepository.java
+++ KDT/src/main/java/org/zerock/b01/repository/SupplierStockRepository.java
@@ -18,6 +18,9 @@ public interface SupplierStockRepository extends JpaRepository<SupplierStock, Lo
     boolean existsBySupplier_sIdAndMaterial_mCode(Long sId, String mCode);
 
     @Query("SELECT ss.supplier.sName FROM SupplierStock ss WHERE ss.material.mCode = :mCode")
+    String findLeadTimeByMCode(@Param("mCode") String mCode);
+
+    @Query("SELECT ss.supplier.sName FROM SupplierStock ss WHERE ss.material.mCode = :mCode")
     List<String> findSNameByMCode(@Param("mCode") String mCode);
 
     @Query("SELECT ss.leadTime FROM SupplierStock ss WHERE ss.supplier.sName=:sName and ss.material.mCode = :mCode")
